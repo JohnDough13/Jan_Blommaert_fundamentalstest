e3f70c7c93e3b592c028a2cdf125e44420fc4980
JohnDough13 <jan.blommaert@student.intecbrussel.be>
2022-01-10 19:00:05 +0100

    First day

--- src/be/intecbrussel/the_notebook/entities/animal_entities/Carnivore.java
+++ src/be/intecbrussel/the_notebook/entities/animal_entities/Carnivore.java
@@ -1,4 +1,30 @@
 package be.intecbrussel.the_notebook.entities.animal_entities;
 
-public class Carnivore {
+public class Carnivore extends Animal{
+
+    protected double maxFoodSize;
+
+    public Carnivore(String name){
+        super(name);
+    }
+
+    public Carnivore(double weight, double height, double length, double maxFoodSize) {
+        super(weight, height, length);
+        this.maxFoodSize = maxFoodSize;
+    }
+
+    public double getMaxFoodSize() {
+        return maxFoodSize;
+    }
+
+    public void setMaxFoodSize(double maxFoodSize) {
+        this.maxFoodSize = maxFoodSize;
+    }
+
+    @Override
+    public String toString() {
+        return "Carnivore{" +
+                "maxFoodSize=" + maxFoodSize +
+                '}';
+    }
 }
