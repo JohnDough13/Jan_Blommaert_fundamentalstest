e5b1e2e1e2d08b3584eba1d0f2a936e40b7367b0
JohnDough13 <jan.blommaert@student.intecbrussel.be>
2022-01-10 19:56:06 +0100

    First day

--- src/be/intecbrussel/the_notebook/app/NatureApp.java
+++ src/be/intecbrussel/the_notebook/app/NatureApp.java
@@ -1,7 +1,15 @@
 package be.intecbrussel.the_notebook.app;
 
+import be.intecbrussel.the_notebook.entities.plant_entities.*;
+import be.intecbrussel.the_notebook.service.ForestNotebook;
+
 public class NatureApp {
     public static void main(String[] args) {
+        ForestNotebook edenInTheMaking = new ForestNotebook();
+        Plant apple = new Tree("Apple tree");
+        Plant maryGold = new Flower("Marygold");
+        Plant haze = new Weed("John d.");
+        Plant blackberries = new Bush("Blake", "blackberries");
 
     }
 }
