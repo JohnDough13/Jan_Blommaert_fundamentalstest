50a9c28fdd7ce13baef2b0e84dd1b263310134bc
JohnDough13 <jan.blommaert@student.intecbrussel.be>
2022-01-11 13:11:08 +0100

    app is working. Still need some fine tuning to do and some instructions to carry out.

--- src/be/intecbrussel/the_notebook/app/NatureApp.java
+++ src/be/intecbrussel/the_notebook/app/NatureApp.java
@@ -1,9 +1,14 @@
 package be.intecbrussel.the_notebook.app;
 
+import be.intecbrussel.the_notebook.entities.animal_entities.Animal;
+import be.intecbrussel.the_notebook.entities.animal_entities.Carnivore;
+import be.intecbrussel.the_notebook.entities.animal_entities.Herbivore;
+import be.intecbrussel.the_notebook.entities.animal_entities.Omnivore;
 import be.intecbrussel.the_notebook.entities.plant_entities.*;
 import be.intecbrussel.the_notebook.service.ForestNotebook;
 
 public class NatureApp {
+
     public static void main(String[] args) {
         ForestNotebook edenInTheMaking = new ForestNotebook();
         Plant apple = new Tree("Apple tree");
@@ -11,5 +16,20 @@ public class NatureApp {
         Plant haze = new Weed("John d.");
         Plant blackberries = new Bush("Blake", "blackberries");
 
+        Animal rabbit = new Herbivore(12,13,14);
+        Animal chicken = new Herbivore("Poulette");
+        Animal chickOne = new Herbivore("Chickie");
+        Animal chickTwo = new Herbivore("Chickie Chickie");
+        Animal chickTree = new Herbivore("Chickie Chickie Chickie");
+        Animal pig = new Omnivore("King Pig");
+        Animal hawk = new Carnivore("Black Hawk");
+
+        System.out.println(rabbit.getHeight());
+        System.out.println(chicken.getName());
+        System.out.println(chickOne.getName());
+        System.out.println(chickTwo.getName());
+        System.out.println(chickTree.getName());
+        System.out.println(pig.getName());
+        System.out.println(hawk.getName());
     }
 }
